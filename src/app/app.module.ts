2729016981dc27ac3a9f1e78442433161be49801
rkarelin <rkarelin@aktivsystems.ru>
2022-03-23 01:53:18 +0300

    - Components/models added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchSettingsComponent } from './search/search-settings/search-settings.component';
+import { HeaderComponent } from './header/header/header.component';
+import { CardOneComponent } from './cards/card-one/card-one.component';
+import { CardTwoComponent } from './cards/card-two/card-two.component';
+import { VideoCardComponent } from './cards/video-card/video-card.component';
+import { CreateCardComponent } from './cards/create-card/create-card.component';
+import { LoginComponent } from './admin/login/login.component';
+import { RegisterComponent } from './admin/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchItemComponent,
+    SearchResultsComponent,
+    SearchSettingsComponent,
+    HeaderComponent,
+    CardOneComponent,
+    CardTwoComponent,
+    VideoCardComponent,
+    CreateCardComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
